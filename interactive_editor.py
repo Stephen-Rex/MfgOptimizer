7af41221feae4d15e0de4f9dcf229e6e083b7ada
Stephen-Rex <80047977+Stephen-Rex@users.noreply.github.com>
2026-08-24 14:18:22 -0400

    Update interactive_editor.py

--- interactive_editor.py
+++ interactive_editor.py
@@ -1298,9 +1298,11 @@ def render_interactive_editor_controls():
 
 def render_interactive_editor():
     _apply_pending_vertex_selection_if_any()
+    _apply_pending_workflow_point_selection_if_any()
 
     if st.session_state.get("editor_prime_inputs", True):
         _prime_editor_inputs_from_selection()
+        _prime_workflow_inputs_from_selection()
         st.session_state.editor_prime_inputs = False
 
     render_interactive_editor_controls()
