670d5b294adb0fac992aeab255ca07db4b3c9f96
Stephen-Rex <80047977+Stephen-Rex@users.noreply.github.com>
2026-08-25 14:59:52 -0400

    Update state_manager.py

--- state_manager.py
+++ state_manager.py
@@ -478,7 +478,16 @@ def init_session_state(machinery_lib, lighting_lib, crane_lib):
     if "editor_trace_map" not in st.session_state:
         st.session_state.editor_trace_map = []
 
-
+    if "editor_move_awaiting_target" not in st.session_state:
+        st.session_state.editor_move_awaiting_target = False
+    if "editor_move_selected_type" not in st.session_state:
+        st.session_state.editor_move_selected_type = ""
+    if "editor_move_selected_index" not in st.session_state:
+        st.session_state.editor_move_selected_index = -1
+    if "editor_move_selected_vertex_index" not in st.session_state:
+        st.session_state.editor_move_selected_vertex_index = -1
+    if "editor_move_selected_workflow_point_index" not in st.session_state:
+        st.session_state.editor_move_selected_workflow_point_index = -1
     
 
     if "placed_machines" not in st.session_state:
@@ -641,20 +650,6 @@ def init_session_state(machinery_lib, lighting_lib, crane_lib):
     if "editor_phase3_status" not in st.session_state:
         st.session_state.editor_phase3_status = ""
 
-    if "editor_move_awaiting_target" not in st.session_state:
-        st.session_state.editor_move_awaiting_target = False
-
-    if "editor_move_selected_type" not in st.session_state:
-        st.session_state.editor_move_selected_type = ""
-
-    if "editor_move_selected_index" not in st.session_state:
-        st.session_state.editor_move_selected_index = -1
-
-    if "editor_move_selected_vertex_index" not in st.session_state:
-        st.session_state.editor_move_selected_vertex_index = -1
-
-    if "editor_move_selected_workflow_point_index" not in st.session_state:
-        st.session_state.editor_move_selected_workflow_point_index = -1
     
 
     ensure_object_ids()
