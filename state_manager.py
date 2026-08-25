df58f49e870980730e631e2c366d791b271cf160
Stephen-Rex <80047977+Stephen-Rex@users.noreply.github.com>
2026-08-25 12:45:10 -0400

    Update state_manager.py

--- state_manager.py
+++ state_manager.py
@@ -460,7 +460,9 @@ def init_session_state(machinery_lib, lighting_lib, crane_lib):
         st.session_state.editor_drag_preview_dx_ft = 0.0
 
     if "editor_drag_preview_dy_ft" not in st.session_state:
-        st.session_state.editor_drag_preview_dy_ft = 0.0        
+        st.session_state.editor_drag_preview_dy_ft = 0.0
+    if "editor_surface_mode" not in st.session_state:
+        st.session_state.editor_surface_mode = "Phase 3 Interactive Canvas"        
     
 
     if "placed_machines" not in st.session_state:
