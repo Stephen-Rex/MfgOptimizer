8557876c8fc04589587c0d38d4cc9630f9ec64e7
Stephen-Rex <80047977+Stephen-Rex@users.noreply.github.com>
2026-08-26 10:35:15 -0400

    Update interactive_editor.py

--- interactive_editor.py
+++ interactive_editor.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import streamlit as st
-from streamlit_plotly_events import plotly_events2
+from streamlit_plotly_events import plotly_events
 from interactive_canvas import build_interactive_canvas_figure, apply_canvas_pick
 
 
